93bbce53cd8195eaca874545fac0dd166778ff53
adamSH91 <adamserraj91@gmail.com>
2023-09-15 14:13:49 +0100

    finished conditionals task 1

---
+++ src/conditionals/ConditionalsExercise.java
@@ -0,0 +1,19 @@
+package conditionals;
+
+public class ConditionalsExercise {
+    public static void main(String[] args) {
+        // Task 1
+        int x = 3, y= 5;
+        if (x >= 0) {
+            if (y < x) {
+                System.out.println("y is less than x");
+            } else if (y > x){
+                System.out.println("y is greater than x");
+            } else {
+                System.out.println("y and x are the same");
+            }
+        } else {
+            System.out.println("x is negative");
+        }
+    }
+}
