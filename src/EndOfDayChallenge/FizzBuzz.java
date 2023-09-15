0838ffe8fb6094feb722fb690ea6871cc48624d9
adamSH91 <adamserraj91@gmail.com>
2023-09-15 17:22:08 +0100

    finished extended fizz buzz

--- src/EndOfDayChallenge/FizzBuzz.java
+++ src/EndOfDayChallenge/FizzBuzz.java
@@ -14,5 +14,33 @@ public class FizzBuzz {
                 System.out.println("Buzz");
             }
         }
+
+        System.out.println("----");
+
+        // Extended fizz buzz
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter a number");
+        int num = scan.nextInt();
+        if (num < 0) {
+            for (int i = 0; i >= num; i--) {
+                if (i % 3 == 0 && i % 5 == 0) {
+                    System.out.println("Fizz Buzz");
+                } else if (i % 3 == 0) {
+                    System.out.println("Fizz");
+                } else if (i % 5 == 0) {
+                    System.out.println("Buzz");
+                }
+            }
+        } else {
+            for (int i = 0; i <= num; i++) {
+                if (i % 3 == 0 && i % 5 == 0) {
+                    System.out.println("Fizz Buzz");
+                } else if (i % 3 == 0) {
+                    System.out.println("Fizz");
+                } else if (i % 5 == 0) {
+                    System.out.println("Buzz");
+                }
+            }
+        }
     }
 }
